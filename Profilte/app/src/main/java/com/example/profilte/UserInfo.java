5739f232a0e3f97829881034c121e7c5c7fd7f29
kimsooyoung <youworthit17@ajou.ac.kr>
2020-06-19 03:04:11 +0900

    그냥 언어쪽 건드려본거

---
+++ Profilte/app/src/main/java/com/example/profilte/UserInfo.java
@@ -0,0 +1,130 @@
+package com.example.profilte;
+
+import com.google.gson.annotations.SerializedName;
+
+public class UserInfo {
+
+    //<----------------------------------------------->//
+    //해본거//수영
+    //<----------------------------------------------->//
+
+
+    @SerializedName("no")
+    public Integer no;
+
+    @SerializedName("id")
+    public String id;
+
+    @SerializedName("password")
+    public String password;
+
+    @SerializedName("nickname")
+    public String nickname;
+
+    @SerializedName("points")
+    public Integer points;
+
+    @SerializedName("money")
+    public Integer money;
+
+    @SerializedName("requestnum")
+    public Integer requestNum;
+
+    @SerializedName("registernum")
+    public Integer regiseterNum;
+
+    @SerializedName("ratingcompleteness")
+    public double ratingCompletenss;
+
+    @SerializedName("ratingclarity")
+    public double ratingClarity;
+
+    @SerializedName("ratingtime")
+    public Integer ratingTime;
+
+    @SerializedName("noticecnt")
+    public Integer noticeCnt;
+
+
+    //ratingCnt
+
+    public Integer getno() {
+        return no;
+    }
+    public void setno(Integer no) {
+        this.no=no;
+    }
+
+    public String getid() {
+        return id;
+    }
+    public void setid(String id) {
+        this.id=id;
+    }
+
+    public String getpw() {
+        return password;
+    }
+    public void setpw(String password) {
+        this.password=password;
+    }
+
+    public String getnn() {
+        return nickname;
+    }
+    public void setnn(String nickname) {
+        this.nickname=nickname;
+    }
+
+    public Integer getpoints() {
+        return points;
+    }
+    public void setpoints(Integer points) {
+        this.points=points;
+    }
+
+    public Integer getmoney() {
+        return money;
+    }
+    public void setmoney(Integer money) {
+        this.money=money;
+    }
+
+    public Integer gerequestNum() {
+        return requestNum;
+    }
+    public void setrequestNum(Integer requestNum) {
+        this.requestNum=requestNum;
+    }
+
+    public Integer getregiseterNum() {
+        return regiseterNum;
+    }
+    public void setregiseterNum(Integer regiseterNum) {
+        this.regiseterNum=regiseterNum;
+    }
+
+    public double getratingCompletenss() {
+        return ratingCompletenss;
+    }
+    public void setratingCompletenss(double ratingCompletenss) { this.ratingCompletenss=ratingCompletenss; }
+
+    public double getratingClarity() {
+        return ratingClarity;
+    }
+    public void setratingClarity(double ratingClarity) { this.ratingClarity=ratingClarity; }
+
+    public Integer getratingTime() {
+        return ratingTime;
+    }
+    public void setratingTime(Integer ratingTime) {
+        this.ratingTime=ratingTime;
+    }
+
+    public Integer getnoticeCnt() {
+        return noticeCnt;
+    }
+    public void setnoticeCnt(Integer noticeCnt) {
+        this.noticeCnt=noticeCnt;
+    }
+}
